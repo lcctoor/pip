80f92ebabe5367271e6e0c5eeb6c8e2205aa6145
lcctoor <lcctoor@outlook.com>
2024-07-29 07:59:16 +0800

    openai2: 支持多模态(gpt-4o)

--- arts/openai2/_core/_chat_in_cmd.py
+++ arts/openai2/_core/_chat_in_cmd.py
@@ -3,15 +3,15 @@ from .chat import Chat, AKPool
 from arts.cooltypes import moduledb
 
 
-mdb = moduledb.DB('https://pypi.org/project/openai2', depth=3)['chat_in_cmd']
+mdb = moduledb.DB('https://pypi.org/project/openai2', depth=3)['_chat_in_cmd']
 apikeys: moduledb.File = mdb['apikeys']['data_1']
 apikeys.setdefault('list', [])
 record: moduledb.File = mdb['records']['chat_1']
 record.setdefault('messages', [])
 
 
-def chat_in_cmd(apikeys:list, newchat=False, model='gpt-4-1106-preview', msg_max_count=30):
-    print(f"\n\033[0m您已进入命令行聊天模式, 当前使用'{model}'模型, 请确保您的apikey支持该模型.", end='')
+def _chat_in_cmd(apikeys:list, newchat=False, model='gpt-4-turbo', msg_max_count=30):
+    print(f"\n\033[0m你已进入命令行聊天模式, 当前使用'{model}'模型, 请确保你的apikey支持该模型.", end='')
     gpt = Chat(api_key=AKPool(apikeys), model=model, msg_max_count=msg_max_count)
     if not newchat:
         gpt.add_dialogs(*record['messages'])
@@ -32,7 +32,7 @@ openai2 newchat             | 清空对话记录, 然后开始新对话 |
 '''
 
 
-def ParseCmd():
+def _ParseCmd():
     kws = sys.argv[1:]
     if kws:
         kw = kws[0].lower()
@@ -45,11 +45,11 @@ def ParseCmd():
 
         # 继续上次的对话
         elif kw == 'chat':
-            chat_in_cmd(apikeys['list'], newchat=False)
+            _chat_in_cmd(apikeys['list'], newchat=False)
         
         # 清空对话记录, 然后开始新对话
         elif kw == 'newchat':
-            chat_in_cmd(apikeys['list'], newchat=True)
+            _chat_in_cmd(apikeys['list'], newchat=True)
         
         # 查看所有apikey
         elif kw == 'read_apikey':
